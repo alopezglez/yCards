4edf6786205e60e8a375438c82b538fefecbf99d
alopezglez <78856028+alopezglez@users.noreply.github.com>
2021-03-07 12:51:04 +0000

    v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace yCards
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.Carta_1 = new System.Windows.Forms.PictureBox();
+            this.Carta_4 = new System.Windows.Forms.PictureBox();
+            this.Carta_3 = new System.Windows.Forms.PictureBox();
+            this.Carta_2 = new System.Windows.Forms.PictureBox();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btReiniciar = new System.Windows.Forms.Button();
+            this.lblMarcador = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.Comparar_2 = new System.Windows.Forms.PictureBox();
+            this.Comparar_1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Comparar_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Comparar_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p1
+            // 
+            this.p1.Image = ((System.Drawing.Image)(resources.GetObject("p1.Image")));
+            this.p1.Location = new System.Drawing.Point(664, 144);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(100, 50);
+            this.p1.TabIndex = 0;
+            this.p1.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.Image = ((System.Drawing.Image)(resources.GetObject("p2.Image")));
+            this.p2.Location = new System.Drawing.Point(672, 152);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(100, 50);
+            this.p2.TabIndex = 1;
+            this.p2.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.Image = ((System.Drawing.Image)(resources.GetObject("p3.Image")));
+            this.p3.Location = new System.Drawing.Point(680, 160);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(100, 50);
+            this.p3.TabIndex = 2;
+            this.p3.TabStop = false;
+            // 
+            // p4
+            // 
+            this.p4.Image = ((System.Drawing.Image)(resources.GetObject("p4.Image")));
+            this.p4.Location = new System.Drawing.Point(688, 168);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(100, 50);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p4.TabIndex = 3;
+            this.p4.TabStop = false;
+            // 
+            // Carta_1
+            // 
+            this.Carta_1.Location = new System.Drawing.Point(12, 68);
+            this.Carta_1.Name = "Carta_1";
+            this.Carta_1.Size = new System.Drawing.Size(100, 126);
+            this.Carta_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Carta_1.TabIndex = 4;
+            this.Carta_1.TabStop = false;
+            this.Carta_1.Click += new System.EventHandler(this.Carta_1_Click);
+            // 
+            // Carta_4
+            // 
+            this.Carta_4.Location = new System.Drawing.Point(443, 68);
+            this.Carta_4.Name = "Carta_4";
+            this.Carta_4.Size = new System.Drawing.Size(100, 126);
+            this.Carta_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Carta_4.TabIndex = 5;
+            this.Carta_4.TabStop = false;
+            this.Carta_4.Click += new System.EventHandler(this.Carta_4_Click);
+            // 
+            // Carta_3
+            // 
+            this.Carta_3.Location = new System.Drawing.Point(299, 68);
+            this.Carta_3.Name = "Carta_3";
+            this.Carta_3.Size = new System.Drawing.Size(100, 126);
+            this.Carta_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Carta_3.TabIndex = 6;
+            this.Carta_3.TabStop = false;
+            this.Carta_3.Click += new System.EventHandler(this.Carta_3_Click);
+            // 
+            // Carta_2
+            // 
+            this.Carta_2.Location = new System.Drawing.Point(158, 68);
+            this.Carta_2.Name = "Carta_2";
+            this.Carta_2.Size = new System.Drawing.Size(100, 126);
+            this.Carta_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Carta_2.TabIndex = 7;
+            this.Carta_2.TabStop = false;
+            this.Carta_2.Click += new System.EventHandler(this.Carta_2_Click);
+            // 
+            // btSalir
+            // 
+            this.btSalir.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btSalir.Location = new System.Drawing.Point(443, 248);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(100, 38);
+            this.btSalir.TabIndex = 8;
+            this.btSalir.Text = "&Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // btReiniciar
+            // 
+            this.btReiniciar.Location = new System.Drawing.Point(326, 248);
+            this.btReiniciar.Name = "btReiniciar";
+            this.btReiniciar.Size = new System.Drawing.Size(100, 38);
+            this.btReiniciar.TabIndex = 9;
+            this.btReiniciar.Text = "&Reiniciar";
+            this.btReiniciar.UseVisualStyleBackColor = true;
+            this.btReiniciar.Click += new System.EventHandler(this.btReiniciar_Click);
+            // 
+            // lblMarcador
+            // 
+            this.lblMarcador.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMarcador.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMarcador.Location = new System.Drawing.Point(12, 243);
+            this.lblMarcador.Name = "lblMarcador";
+            this.lblMarcador.Size = new System.Drawing.Size(100, 38);
+            this.lblMarcador.TabIndex = 10;
+            this.lblMarcador.Text = "2";
+            this.lblMarcador.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(531, 43);
+            this.label1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(20, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 35);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "yCards";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(354, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 35);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "v0.1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(12, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(531, 16);
+            this.label4.TabIndex = 14;
+            // 
+            // p5
+            // 
+            this.p5.Image = ((System.Drawing.Image)(resources.GetObject("p5.Image")));
+            this.p5.Location = new System.Drawing.Point(698, 176);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(100, 50);
+            this.p5.TabIndex = 15;
+            this.p5.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Snap ITC", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResult.Location = new System.Drawing.Point(12, 52);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(531, 158);
+            this.lblResult.TabIndex = 16;
+            this.lblResult.Text = "Perfecto. Conseguido!!";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResult.Visible = false;
+            // 
+            // Comparar_2
+            // 
+            this.Comparar_2.Image = ((System.Drawing.Image)(resources.GetObject("Comparar_2.Image")));
+            this.Comparar_2.Location = new System.Drawing.Point(717, 237);
+            this.Comparar_2.Name = "Comparar_2";
+            this.Comparar_2.Size = new System.Drawing.Size(100, 50);
+            this.Comparar_2.TabIndex = 17;
+            this.Comparar_2.TabStop = false;
+            // 
+            // Comparar_1
+            // 
+            this.Comparar_1.Image = ((System.Drawing.Image)(resources.GetObject("Comparar_1.Image")));
+            this.Comparar_1.Location = new System.Drawing.Point(596, 236);
+            this.Comparar_1.Name = "Comparar_1";
+            this.Comparar_1.Size = new System.Drawing.Size(100, 50);
+            this.Comparar_1.TabIndex = 18;
+            this.Comparar_1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 299);
+            this.Controls.Add(this.Comparar_1);
+            this.Controls.Add(this.Comparar_2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMarcador);
+            this.Controls.Add(this.btReiniciar);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.Carta_2);
+            this.Controls.Add(this.Carta_3);
+            this.Controls.Add(this.Carta_4);
+            this.Controls.Add(this.Carta_1);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "yCards @ 2021, Alexander López González";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carta_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Comparar_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Comparar_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox Carta_1;
+        private System.Windows.Forms.PictureBox Carta_4;
+        private System.Windows.Forms.PictureBox Carta_3;
+        private System.Windows.Forms.PictureBox Carta_2;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button btReiniciar;
+        private System.Windows.Forms.Label lblMarcador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox Comparar_2;
+        private System.Windows.Forms.PictureBox Comparar_1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yCards
+{
+    public partial class Form1 : Form
+    {
+
+        int c1;
+        int c2;
+        int c3;
+        int c4;
+        int CartaArriba_1;
+        int CartaArriba_2;
+        int CartasArriba = 0;
+        int Intentos = 2;
+        string Ganado = "Perfecto - Conseguido !!";
+        string Perdido = "Vaya - Has Perdido !!";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btReiniciar_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            c1 = rnd.Next(0, 4);
+            c2 = rnd.Next(0, 4);
+            c3 = rnd.Next(0, 4);
+            c4 = rnd.Next(0, 4);
+
+            while (c2 == c1)
+            {
+                c2 = rnd.Next(0, 4);
+            }
+
+            while (c3 == c2 || c3 == c1)
+            {
+                c3 = rnd.Next(0, 4);
+            }
+
+            while (c4 != c3 && c4 != c2 && c4 != c1)
+            {
+                c4 = rnd.Next(0, 4);
+            }
+
+            //(Re)inicializa variables y contador
+            Intentos = 2;
+            lblMarcador.Text = Intentos.ToString();
+            CartasArriba = 0;
+
+            VoltearCartas();
+
+
+        }
+
+        private void Carta_1_Click(object sender, EventArgs e)
+        {
+
+            switch (c1)
+            {
+                case 0:
+                    Carta_1.Image = p1.Image;
+                    break;
+
+                case 1:
+                    Carta_1.Image = p2.Image;
+                    break;
+
+                case 2:
+                    Carta_1.Image = p3.Image;
+                    break;
+
+                case 3:
+                    Carta_1.Image = p4.Image;
+                    break;
+
+            }
+
+            if (CartasArriba == 0)
+            {
+                CartaArriba_1 = 1; //Si no hay cartas hacia arriba esta será la primera
+            }
+            else if (CartasArriba == 1)
+            {
+                CartaArriba_2 = 1; //Si ya hay otra carta hacia arriba esta será la segunda
+            }
+
+            ContarCartas();
+
+        }
+
+        private void Carta_2_Click(object sender, EventArgs e)
+        {
+
+            switch (c2)
+            {
+                case 0:
+                    Carta_2.Image = p1.Image;
+                    break;
+
+                case 1:
+                    Carta_2.Image = p2.Image;
+                    break;
+
+                case 2:
+                    Carta_2.Image = p3.Image;
+                    break;
+
+                case 3:
+                    Carta_2.Image = p4.Image;
+                    break;
+
+            }
+
+            if (CartasArriba == 0)
+            {
+                CartaArriba_1 = 2; //Si no hay cartas hacia arriba esta será la primera
+            }
+            else if (CartasArriba == 1)
+            {
+                CartaArriba_2 = 2; //Si ya hay otra carta hacia arriba esta será la segunda
+            }
+
+            ContarCartas();
+
+        }
+
+        private void Carta_3_Click(object sender, EventArgs e)
+        {
+
+            switch (c3)
+            {
+                case 0:
+                    Carta_3.Image = p1.Image;
+                    break;
+
+                case 1:
+                    Carta_3.Image = p2.Image;
+                    break;
+
+                case 2:
+                    Carta_3.Image = p3.Image;
+                    break;
+
+                case 3:
+                    Carta_3.Image = p4.Image;
+                    break;
+
+            }
+
+            if (CartasArriba == 0)
+            {
+                CartaArriba_1 = 3; //Si no hay cartas hacia arriba esta será la primera
+            }
+            else if (CartasArriba == 1)
+            {
+                CartaArriba_2 = 3; //Si ya hay otra carta hacia arriba esta será la segunda
+            }
+
+            ContarCartas();
+
+        }
+
+        private void Carta_4_Click(object sender, EventArgs e)
+        {
+
+            switch (c4)
+            {
+                case 0:
+                    Carta_4.Image = p1.Image;
+                    break;
+
+                case 1:
+                    Carta_4.Image = p2.Image;
+                    break;
+
+                case 2:
+                    Carta_4.Image = p3.Image;
+                    break;
+
+                case 3:
+                    Carta_4.Image = p4.Image;
+                    break;
+
+            }
+
+            if (CartasArriba == 0)
+            {
+                CartaArriba_1 = 4; //Si no hay cartas hacia arriba esta será la primera
+            }
+            else if (CartasArriba == 1)
+            {
+                CartaArriba_2 = 4; //Si ya hay otra carta hacia arriba esta será la segunda
+            }
+
+            ContarCartas();
+
+        }
+
+        void VoltearCartas()
+        {
+            Carta_1.Image = p5.Image;
+            Carta_2.Image = p5.Image;
+            Carta_3.Image = p5.Image;
+            Carta_4.Image = p5.Image;
+            lblResult.Visible = false;
+
+        }
+
+        void ContarCartas()
+        {
+            CartasArriba++;
+            Application.DoEvents(); //Obliga a la segunda carta a destaparse, la función Sleep congela la App
+
+
+            // Si ya hay dos cartas al descubierto, hace comprobaciones
+            if (CartasArriba >= 2)
+            {
+                //Resta un intento
+                Intentos--;
+                lblMarcador.Text = Intentos.ToString();
+
+                //Pequeña pausa para queno sea tan instantaneo
+                System.Threading.Thread.Sleep(2000);
+
+                switch (CartaArriba_1)
+                {
+                    case 1:
+                        Comparar_1.Image = Carta_1.Image;
+                        break;
+                    case 2:
+                        Comparar_1.Image = Carta_2.Image;
+                        break;
+                    case 3:
+                        Comparar_1.Image = Carta_3.Image;
+                        break;
+                    case 4:
+                        Comparar_1.Image = Carta_4.Image;
+                        break;
+
+                }
+
+                switch (CartaArriba_2)
+                {
+                    case 1:
+                        Comparar_2.Image = Carta_1.Image;
+                        break;
+                    case 2:
+                        Comparar_2.Image = Carta_2.Image;
+                        break;
+                    case 3:
+                        Comparar_2.Image = Carta_3.Image;
+                        break;
+                    case 4:
+                        Comparar_2.Image = Carta_4.Image;
+                        break;
+
+                }
+
+                if (Comparar_1.Image == Comparar_2.Image)
+                {
+                    lblResult.Visible = true;
+                    lblResult.Text = Ganado;
+
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(2000);
+
+                    btReiniciar.PerformClick();
+                }
+                else
+                {
+                    if (Intentos == 0)
+                    {
+                        lblResult.Visible = true;
+                        lblResult.Text = Perdido;
+
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(2000);
+
+                        btReiniciar.PerformClick();
+
+                    }
+                }
+
+                // Vuelve a voltear las cartas y reinicia el contador
+
+                CartasArriba = 0;
+                VoltearCartas();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btReiniciar.PerformClick();
+        }
+    }
+}
